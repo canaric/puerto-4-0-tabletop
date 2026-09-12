e11454838502e5c001d74e9404931e15c1bc3ef0
Mg. Ing. Ricardo Alcides Canaveri <canaric@gmail.com>
2026-09-12 17:33:03 -0300

    Add basePath and assetPrefix to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const repositoryName = "puerto-4-0-tabletop";
+const basePath =
+  process.env.NODE_ENV === "production" ? `/${repositoryName}` : "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath,
+  assetPrefix: basePath,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
